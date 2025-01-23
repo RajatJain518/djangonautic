fbfdccadaeee1e8dc414e9e84bfe703e434f5c5d
rajatjain518 <jainrajat612@gmail.com>
2025-01-23 18:27:11 +0530

    changed setings to settingss

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url,include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
-from django.conf import settings
+from django.conf import settingss
 from articles import views as article_views
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
